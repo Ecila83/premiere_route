3f36fd845aea4441e1b5fdf26971f4c94b6b9214
Alice Marique <mariquealice83@gmail.com>
2024-04-04 18:57:36 +0200

    exercice 2

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,10 +6,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController {
-
-    #[route('/accueil', name: 'accueil')]
+    #[route('/blog', name: 'blog', methods:['POST'])]
     public function index()
     {
-        return new Response('<h1>Bienvenue</h1>');
+        return new Response('<h1>Article créé</h1>');
     }
+
+// exercice 1
+    // #[route('/accueil', name: 'accueil')]
+    // public function index()
+    // {
+    //     return new Response('<h1>Bienvenue</h1>');
+    // }
 }
