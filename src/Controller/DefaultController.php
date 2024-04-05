b4f1e3ff585b8dab987c520b5fba7d3399913487
Alice Marique <mariquealice83@gmail.com>
2024-04-05 10:18:45 +0200

    exercice 3

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,12 +5,24 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class DefaultController {
-    #[route('/blog', name: 'blog', methods:['POST'])]
-    public function index()
-    {
-        return new Response('<h1>Article créé</h1>');
-    }
+    #[Route('/animal/{animal?}', name: 'animal', methods:['GET'])]
+    public function animal(string $animal = null)
+    {   
+        if ($animal !== null) {
+            return new Response('<h1>Animal : ' . $animal . '</h1>');
+        } else {
+            return new Response('<h1>Tous les animaux</h1>');        
+        }
+}
+
+// exercice 2
+    // #[route('/blog', name: 'blog', methods:['POST'])]
+    // public function index()
+    // {
+    //     return new Response('<h1>Article créé</h1>');
+    // }
 
 // exercice 1
     // #[route('/accueil', name: 'accueil')]
