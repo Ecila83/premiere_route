a102f3cee6e8bede9d9ef7db8bc3b79d1a52ec56
Alice Marique <mariquealice83@gmail.com>
2024-04-05 11:25:20 +0200

    exercice 4

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,19 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class DefaultController {
-    #[Route('/animal/{animal?}', name: 'animal', methods:['GET'])]
-    public function animal(string $animal = null)
-    {   
-        if ($animal !== null) {
-            return new Response('<h1>Animal : ' . $animal . '</h1>');
-        } else {
-            return new Response('<h1>Tous les animaux</h1>');        
+    
+// exercice 4
+    #[Route('/blog/{title}', name: 'blog', methods:['GET'],requirements:['title' => '[\p{L}\-]+'])]
+    public function blog(string $title){
+        if (!preg_match('/^[\p{L}_\-]+$/u', $title)) {
+            return new JsonResponse(['error' => 'Paramètre de titre invalide.'], Response::HTTP_BAD_REQUEST);
         }
-}
+        $title = str_replace('-', ' ',$title);
+        return new Response('<h1>Titre : ' . $title . '</h1>');
+    }
+
+// exercice 3
+    // #[Route('/animal/{animal?}', name: 'animal', methods:['GET'])]
+    // public function animal(string $animal = null)
+    // {   
+    //     if ($animal !== null) {
+    //         return new Response('<h1>Animal : ' . $animal . '</h1>');
+    //     } else {
+    //         return new Response('<h1>Tous les animaux</h1>');        
+    //     }
+    //  }
 
 // exercice 2
     // #[route('/blog', name: 'blog', methods:['POST'])]
